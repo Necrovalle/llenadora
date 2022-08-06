145a865b305c3c614e007171f9d57b144809b890
Miguel Orozco <39131275+Necrovalle@users.noreply.github.com>
2022-08-06 13:38:52 -0600

    Update llenadora.py
    Con inicialización de comunicación serie

--- llenadora.py
+++ llenadora.py
@@ -4,16 +4,40 @@
 #* VERSION: 0.1alpha
 #* REPOSITORIO:
 #* <URL de gitHub>
+#* Notas: libreria del serial: pip install pyserial
 #*******************************************************************************
 
 from tkinter import *		# Widgets estandar
 from tkinter import ttk		# Widgets nuevos del 8.5+
+from tkinter import messagebox
+import time
 import datetime
+import serial
+
+ser = serial.Serial('COM4')  # open serial port
+print(ser.name)
+
+def Conexion(): 
+    ser.write(b'I')
+    CON = 1
+    time.sleep(1)
+    ENT = 'O'
+    if ser.inWaiting()>0:
+        ENT = ser.read()
+        print(ENT)
+        if ENT != b'I':
+            messagebox.showinfo(message="El hardware no coincide", title="Error")
+        else:
+            #print("Conectado")
+    else:
+        messagebox.showinfo(message="Sin conexión con el hardware", title="Error")
+    
+
 
 def clock():
-        date_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        lbl5.config(text = date_time)
-        lbl5.after(1000, clock)
+    date_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    lbl5.config(text = date_time)
+    lbl5.after(1000, clock)
 
 window = Tk()
 window.geometry('1250x550')
@@ -44,6 +68,7 @@ lbl4.place(x=640, y=300)
 lbl_img.place(x=740, y=410)
 lbl5.place(x=10, y=467)
 
+time.sleep(2)
 clock()
-
+Conexion()
 window.mainloop()
