898b4e9ce8522082ac7560896a6645ec7d6d8e12
Miguel Orozco <39131275+Necrovalle@users.noreply.github.com>
2022-08-07 20:24:44 -0600

    Update llenadora.py
    linea A programada a 6 seg

--- llenadora.py
+++ llenadora.py
@@ -7,6 +7,7 @@
 #* Notas: libreria del serial: pip install pyserial
 #*******************************************************************************
 
+#********************************************************************* LIBRERIAS
 from tkinter import *		# Widgets estandar
 from tkinter import ttk		# Widgets nuevos del 8.5+
 from tkinter import messagebox
@@ -14,9 +15,16 @@ import time
 import datetime
 import serial
 
+#*********************************** DECLARACIONES E INICIALIZACION DE VARIABLES
 ser = serial.Serial('COM4')  # open serial port
-print(ser.name)
+ENT = 'O'   #buffer de la lectura serial
+T_A = 6     #Tiempo en segundos de llenado de la linea A
+T_B = 6     #Tiempo en segundos de llenado de la linea B
+Ta_A = 0    #Tiempo actual en la linea A
+Ta_B = 0    #Tiempo actual en la linea A
+ACT = False #Estado del sistema
 
+#************************************************************* FUNCIONES PROPIAS
 def Conexion(): 
     ser.write(b'I')
     CON = 1
@@ -24,21 +32,44 @@ def Conexion():
     ENT = 'O'
     if ser.inWaiting()>0:
         ENT = ser.read()
-        print(ENT)
         if ENT != b'I':
             messagebox.showinfo(message="El hardware no coincide", title="Error")
-        else:
+        #else:
             #print("Conectado")
     else:
         messagebox.showinfo(message="Sin conexión con el hardware", title="Error")
-    
-
 
 def clock():
+    global ACT
+    global T_A
+    global T_B
+    global Ta_A
+    global Ta_B
     date_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     lbl5.config(text = date_time)
+    if ser.inWaiting()>0:
+        ENT = ser.read()
+        if ENT == b'1':
+            if ACT == False:
+                ACT = True
+                Ta_A = 0
+                Ta_B = 0
+                #calcular tiempos
+        elif ENT == b'0':
+            ACT = False
+    else:
+        ENT = 'O'
+    if ACT == True:
+        if Ta_A == 0:
+            ser.write(b'a')
+        elif Ta_A == T_A:
+            ser.write(b'A')
+        Ta_A += 1
+        if Ta_A > 2*T_A:
+            Ta_A = 0
     lbl5.after(1000, clock)
 
+#******************************************************** CREACION DE LA VENTANA
 window = Tk()
 window.geometry('1250x550')
 window.resizable(False, False)
@@ -68,7 +99,8 @@ lbl4.place(x=640, y=300)
 lbl_img.place(x=740, y=410)
 lbl5.place(x=10, y=467)
 
+#************************************************************* MANEJO DE VENTANA
 time.sleep(2)
-clock()
 Conexion()
+clock()
 window.mainloop()
